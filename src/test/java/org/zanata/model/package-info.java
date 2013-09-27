c1dd138639f2b465aee458023acec6f585452364
Patrick Huang <pahuang@redhat.com>
2013-09-27 10:31:59 +1000

    add javadoc to zanata package info

--- src/test/java/org/zanata/model/package-info.java
+++ src/test/java/org/zanata/model/package-info.java
@@ -1,3 +1,7 @@
 package org.zanata.model;
+/**
+ * This package is taken and modified (removed cache and hibernate search annotation) from
+ * {@link <a href="https://github.com/zanata/zanata-server/tree/master/zanata-model">zanata-model</a>} version 3.1-SNAPSHOT.
+ *
+ */
 
-// this package is taken and modified from zanata-modal version 3.1-SNAPSHOT
